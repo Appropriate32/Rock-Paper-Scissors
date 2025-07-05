ffa42e62d172c3a3c43892c632685db11cf3c934
Appropriate32 <frostghost32@gmail.com>
2025-07-05 23:53:53 +0500

    added some UI with dynamic score display :D

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function getComputerChoice() {
    let numChoice = parseInt(getRandom(1,3)) //Gets a random number between 1 and 3 inclusive
    if (numChoice == 1) {
-    return "rock"
+    return "rock";
    } else if (numChoice == 2) {
-    return "paper"
+    return "paper";
    } else if (numChoice == 3) {
-    return "scissors"
+    return "scissors";
    }
 }
 
@@ -16,52 +16,62 @@ function getRandom(min, max) {  //Generates a random number between min and max 
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-function getHumanChoice() {
-    let choice = prompt("Enter your choice:")
-    return choice
-}
+let humanScore = 0, computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+      humanChoice = humanChoice.toLowerCase();
+      computerChoice = computerChoice.toLowerCase();
+
+      const result = document.querySelector(".result");
+      const display = document.querySelector(".display");
+      const winStatement = document.querySelector(".winStatement");
+      let showYourScore = document.querySelector(".yourScore");
+      let showComputerScore = document.querySelector(".computerScore");
+      let humanUse = document.querySelector(".humanUse");
+      let computerUse = document.querySelector(".computerUse");
 
 
-
-function playGame() {
-   let humanScore = 0, computerScore = 0
-   function playRound(humanChoice, computerChoice) {
-      humanChoice = humanChoice.toLowerCase()
-      computerChoice = computerChoice.toLowerCase()
-      
-      if ((humanChoice == "rock" && computerChoice == "paper") || (humanChoice == "paper" && computerChoice == "scissors") || (humanChoice == "scissors" && computerChoice == "rock")) {
-        console.log("You lose! " + computerChoice + " beats " + humanChoice + "!")
+      if ((humanChoice == "rock" && computerChoice == "paper" && humanScore < 5 && computerScore < 5) || (humanChoice == "paper" && computerChoice == "scissors" && humanScore < 5 && computerScore < 5) || (humanChoice == "scissors" && computerChoice == "rock" && humanScore < 5 && computerScore < 5)) {
+        humanUse.textContent = `You used ${humanChoice}!`;
+        computerUse.textContent = `Computer used ${computerChoice}!`;
+        result.textContent = "You lose! " + computerChoice + " beats " + humanChoice + "!";
         computerScore++
-      } else if ((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "paper" && computerChoice == "rock") || (humanChoice == "scissors" && computerChoice == "paper")) {
-        console.log("You win! " + humanChoice + " beats " + computerChoice + "!")
-        humanScore++
+      } else if ((humanChoice == "rock" && computerChoice == "scissors" && humanScore < 5 && computerScore < 5) || (humanChoice == "paper" && computerChoice == "rock" && humanScore < 5 && computerScore < 5) || (humanChoice == "scissors" && computerChoice == "paper" && humanScore < 5 && computerScore < 5)) {
+        humanUse.textContent = `You used ${humanChoice}!`;
+        computerUse.textContent = `Computer used ${computerChoice}!`;
+        result.textContent = "You win! " + humanChoice + " beats " + computerChoice + "!";
+        humanScore++;
       } else {
-        console.log("It's a draw!")
+        humanUse.textContent = `You used ${humanChoice}!`;
+        computerUse.textContent = `Computer used ${computerChoice}!`;
+        result.textContent = "It's a draw!";
       }
-
+      
+      showYourScore.textContent = `Your Score: ${humanScore}`;
+      showComputerScore.textContent = `Computer Score: ${computerScore}`;
+      if (humanScore == 5) {
+        winStatement.textContent = `You Win!!!`;
+      } else if (computerScore == 5) {
+        winStatement.textContent = `You Lose!!! Computer Wins.`;
+      }
 }
 
-for (let i = 1; i <= 5; i++) {
-    let humanSelection = getHumanChoice()
-    let computerSelection = getComputerChoice()
-    playRound(humanSelection, computerSelection)
-}
-console.log("")
-if (humanScore > computerScore) {
-    console.log("You Win!!!")
-    console.log("Your score was: " + humanScore + ", and opponent's score was: " + computerScore)
-} else if (computerScore > humanScore) {
-    console.log("You Lose!!!")
-    console.log("Your score was: " + humanScore + ", and opponent's score was: " + computerScore)
-} else {
-    console.log("It was a draw!!!")
-    console.log("Your score was: " + humanScore + ", and opponent's score was: " + computerScore)
-}
-}
+const rock = document.querySelector(".rock");
+rock.addEventListener("click", () => {
+  playRound("rock", getComputerChoice());
+});
+
+const paper = document.querySelector(".paper");
+paper.addEventListener("click", () => {
+  playRound("paper", getComputerChoice());
+});
+
+const scissors = document.querySelector(".scissors");
+scissors.addEventListener("click", () => {
+  playRound("scissors", getComputerChoice());
+});
 
 
 
-
-playGame()
 
 
